d392b0857174224ef874505d3d44c8625fcd628c
elvision <elvisiondragon@gmail.com>
2026-02-18 12:50:29 +0700

    soe

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,21 @@
+const APP_VERSION = '2026.02.17.01'; // <-- Change this number to force an update
+
+if (typeof window !== 'undefined' && localStorage.getItem('v_cache') !== APP_VERSION) {
+  // 1. Clear all Service Workers
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(regs => regs.forEach(r => r.unregister()));
+  }
+
+  // 2. Clear all Browser Caches
+  if ('caches' in window) {
+    caches.keys().then(names => names.forEach(n => caches.delete(n)));
+  }
+
+  // 3. Update version and Hard Reload
+  localStorage.setItem('v_cache', APP_VERSION);
+  window.location.reload();
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
